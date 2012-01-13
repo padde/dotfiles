e7bc04fb131800554aa759bc1eec47e90e51c6ba
Patrick Oscity <patrick.oscity@gmail.com>
2012-01-13 13:37:07 +0100

    zsh: add textmate functions & aliases

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -1,3 +1,25 @@
 alias bye='exit'
-alias dotfiles="mate ~/.dotfiles"
 alias lla='ll -a'
+alias dotfiles="mate ~/.dotfiles"
+
+# textmate
+function mate_arg {
+	if test -z "$1"
+	then
+	  mate .
+	else
+	  mate $1
+	fi
+}
+alias m='mate_arg'
+
+# textmate 2
+function mate2_arg {
+	if test -z "$1"
+	then
+	  mate2 .
+	else
+	  mate2 $1
+	fi
+}
+alias m2='mate2_arg'
