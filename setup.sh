64cc04fd516aedd2df90ecc28d395f7e11c4258f
Patrick Oscity <patrick.oscity@gmail.com>
2012-05-05 13:27:28 +0200

    fix setup script (again...)

--- setup.sh
+++ setup.sh
@@ -1,34 +1,26 @@
 echo "Installing Padde’s dotfiles..."
 
-# move containing directory to ~/.dotfiles and go there
-mv $(cd "$(dirname "$0")"; pwd) ~/.dotfiles
-cd ~/.dotfiles
-
-function link_file() {
-  if [ -a $2 ]; then        # file already exists
-    echo -n "backing up "
-    mv -v "$2" "$2.backup"  # make backup
-  fi
-  ln -vs "~/.dotfiles/$1" "$2"
-}
-
-# create symlinks
-link_file gem/gemrc         ~/.gemrc
-link_file git/gitconfig     ~/.gitconfig
-link_file git/gitignore     ~/.gitignore
-link_file input/inputrc     ~/.inputrc
-link_file irb/irbrc         ~/.irbrc
-link_file latexmk/latexmkrc ~/.latexmkrc
-link_file vim/vimrc         ~/.vimrc
-link_file vim/vim/          ~/.vim
-link_file zsh/zsh/          ~/.zsh
-link_file zsh/zshrc         ~/.zshrc
-link_file zsh/oh-my-zsh/    ~/.oh-my-zsh
-
-# update dotfiles and fetch git submodules
+# update dotfiles and fetch submodules
 /usr/bin/env git pull origin master
 /usr/bin/env git submodule init
 /usr/bin/env git submodule update
+
+# move containing directory to ~/.dotfiles and go there
+cp -r . ~/.dotfiles
+cd ~/.dotfiles
+
+# create symlinks
+ln -vs ~/.dotfiles/gem/gemrc         ~/.gemrc
+ln -vs ~/.dotfiles/git/gitconfig     ~/.gitconfig
+ln -vs ~/.dotfiles/git/gitignore     ~/.gitignore
+ln -vs ~/.dotfiles/input/inputrc     ~/.inputrc
+ln -vs ~/.dotfiles/irb/irbrc         ~/.irbrc
+ln -vs ~/.dotfiles/latexmk/latexmkrc ~/.latexmkrc
+ln -vs ~/.dotfiles/vim/vimrc         ~/.vimrc
+ln -vs ~/.dotfiles/vim/vim/          ~/.vim
+ln -vs ~/.dotfiles/zsh/zsh/          ~/.zsh
+ln -vs ~/.dotfiles/zsh/zshrc         ~/.zshrc
+ln -vs ~/.dotfiles/zsh/oh-my-zsh/    ~/.oh-my-zsh
 
 # install oh-my-zsh
 chsh -s `which zsh`
