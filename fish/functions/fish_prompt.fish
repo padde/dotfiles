2fe9be2d54ab172b1fa66ed0e5eb3016f6b24f39
Patrick Oscity <patrick.oscity@gmail.com>
2013-09-23 09:58:46 +0200

    FISH: slightly faster prompt by reducing number of printfs

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,24 +1,35 @@
 function fish_prompt
-  set_color black
-  printf '%s ' (date "+%H:%M:%S")
+  if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
 
-  set_color blue
-  printf '%s' $USER
+  if not set -q __fish_prompt_color_delimiter
+    set -g __fish_prompt_color_delimiter (set_color black)
+  end
 
-  set_color black
-  printf '@'
+  if not set -q __fish_prompt_color_username
+    set -g __fish_prompt_color_username (set_color blue)
+  end
 
-  set_color blue
-  printf '%s' (hostname|cut -d . -f 1)
+  if not set -q __fish_prompt_color_hostname
+    set -g __fish_prompt_color_hostname (set_color blue)
+  end
 
-  set_color black
-  printf ':'
+  if not set -q __fish_prompt_color_pwd
+    set -g __fish_prompt_color_pwd (set_color red)
+  end
 
-  set_color red
-  printf '%s' (prompt_pwd)
+  if not set -q __fish_prompt_color_normal
+    set -g __fish_prompt_color_normal (set_color normal)
+  end
 
-  set_color black
-  printf '$ '
-
-  set_color normal
+  printf '%s%s %s%s%s%s%s%s%s%s%s%s%s%s%s ' \
+    $__fish_prompt_color_delimiter (date "+%H:%M:%S") \
+    $__fish_prompt_color_username  $USER \
+    $__fish_prompt_color_delimiter '@' \
+    $__fish_prompt_color_hostname  $__fish_prompt_hostname \
+    $__fish_prompt_color_delimiter ':' \
+    $__fish_prompt_color_pwd       (prompt_pwd) \
+    $__fish_prompt_color_delimiter '$' \
+    $__fish_prompt_color_normal
 end
--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,111 +1,113 @@
-function __fast_find_git_root
-  git rev-parse --show-toplevel > /dev/null 2>&1
-end
-
-function __fast_git_dirty
-  test (count (git status --porcelain)) != 0
-end
-
-function __fast_git_branch
-  git rev-parse --abbrev-ref HEAD
-end
-
-function __fast_git_shorthash
-  git log -1 --format="%h"
-end
-
-
-
-function __fast_find_hg_root
-  set -l dir (pwd)
-  set -e HG_ROOT
-
-  while test $dir != "/"
-    if test -f $dir'/.hg/dirstate'
-      set -g HG_ROOT $dir"/.hg"
-      return 0
-    end
-    set -l dir (dirname $dir)
-  end
-
-  return 1
-end
-
-function __fast_hg_dirty
-  test (count (hg status --cwd $HG_ROOT)) != 0
-end
-
-function __fast_hg_branch
-  cat "$HG_ROOT/branch" 2>/dev/null
-  or hg branch
-end
-
-function __fast_hg_shorthash
-  hexdump -n 4 -e '1/1 "%02x"' "$HG_ROOT/dirstate" | cut -c-7
-end
-
-
-
 function fish_right_prompt
   set last_status $status
 
-  set_color black
+  function __fast_find_git_root
+    git rev-parse --show-toplevel > /dev/null 2>&1
+  end
+
+  function __fast_git_dirty
+    test (count (git status --porcelain)) != 0
+  end
+
+  function __fast_git_branch
+    git rev-parse --abbrev-ref HEAD
+  end
+
+  function __fast_git_hash
+    git log -1 --format="%h"
+  end
+
+  function __fast_find_hg_root
+    set -l dir (pwd)
+    set -e HG_ROOT
+
+    while test $dir != "/"
+      if test -f $dir'/.hg/dirstate'
+        set -g HG_ROOT $dir"/.hg"
+        return 0
+      end
+      set -l dir (dirname $dir)
+    end
+
+    return 1
+  end
+
+  function __fast_hg_dirty
+    test (count (hg status --cwd $HG_ROOT)) != 0
+  end
+
+  function __fast_hg_branch
+    cat "$HG_ROOT/branch" 2>/dev/null
+    or hg branch
+  end
+
+  function __fast_hg_hash
+    hexdump -n 4 -e '1/1 "%02x"' "$HG_ROOT/dirstate" | cut -c-7
+  end
+
+
+  if not set -q __fish_prompt_color_delimiter
+    set -g __fish_prompt_color_delimiter (set_color black)
+  end
+
+  if not set -q __fish_prompt_color_laststatus
+    set -g __fish_prompt_color_laststatus (set_color red)
+  end
+
+  if not set -q __fish_prompt_color_vcs_dirty
+    set -g __fish_prompt_color_vcs_dirty (set_color red)
+  end
+
+  if not set -q __fish_prompt_color_vcs_branch
+    set -g __fish_prompt_color_vcs_branch (set_color green)
+  end
+
+  if not set -q __fish_prompt_color_vcs_hash
+    set -g __fish_prompt_color_vcs_hash (set_color yellow)
+  end
+
+  if not set -q __fish_prompt_color_normal
+    set -g __fish_prompt_color_normal (set_color normal)
+  end
 
   if test $last_status != 0
-    set_color red
-    printf "⚡$last_status "
+    printf '%s⚡%s' \
+      $__fish_prompt_color_laststatus $last_status
   end
 
   if __fast_find_git_root
-    set_color black
-    printf '['
-
     if __fast_git_dirty
-      set_color red
-      printf '●'
+      set -g __fish_prompt_git_dirty_str '●'
+    else
+      set -e __fish_prompt_git_dirty_str
     end
 
-    set_color black
-    printf 'git:'
-
-    set_color green
-    printf '%s' (__fast_git_branch)
-
-    set_color black
-    printf '@'
-
-    set_color yellow
-    printf '%s' (__fast_git_shorthash)
-
-    set_color black
-    printf ']'
+    printf ' %s%s%s%s%s%s%s%s%s%s%s%s%s%s%s' \
+      $__fish_prompt_color_delimiter  '[' \
+      $__fish_prompt_color_vcs_dirty  $__fish_prompt_git_dirty_str \
+      $__fish_prompt_color_delimiter  'git:' \
+      $__fish_prompt_color_vcs_branch (__fast_git_branch) \
+      $__fish_prompt_color_delimiter  '@' \
+      $__fish_prompt_color_vcs_hash   (__fast_git_hash) \
+      $__fish_prompt_color_delimiter  ']' \
+      $__fish_prompt_color_normal
   end
-
 
   if __fast_find_hg_root
-    set_color black
-    printf '['
-
     if __fast_hg_dirty
-      set_color red
-      printf '●'
+      set -g __fish_prompt_hg_dirty_str '●'
+    else
+      set -e __fish_prompt_hg_dirty_str
     end
 
-    set_color black
-    printf 'hg:'
-
-    set_color green
-    printf '%s' (__fast_hg_branch)
-
-    set_color black
-    printf '@'
-
-    set_color yellow
-    printf '%s' (__fast_hg_shorthash)
-
-    set_color black
-    printf ']'
+    printf ' %s%s%s%s%s%s%s%s%s%s%s%s%s%s%s' \
+      $__fish_prompt_color_delimiter  '[' \
+      $__fish_prompt_color_vcs_dirty  $__fish_prompt_hg_dirty_str \
+      $__fish_prompt_color_delimiter  'hg:' \
+      $__fish_prompt_color_vcs_branch (__fast_hg_branch) \
+      $__fish_prompt_color_delimiter  '@' \
+      $__fish_prompt_color_vcs_hash   (__fast_hg_hash) \
+      $__fish_prompt_color_delimiter  ']' \
+      $__fish_prompt_color_normal
   end
-
-  set_color normal
 end
