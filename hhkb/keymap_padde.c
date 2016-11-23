ce69ca6121f9c24c919b287356959e3fbc3cb04f
Patrick Oscity <patrick.oscity@zweitag.de>
2016-11-23 14:05:39 +0100

    HHKB add keymap for TMK controller

---
+++ hhkb/keymap_padde.c
@@ -0,0 +1,32 @@
+#include "keymap_common.h"
+
+const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
+    /* Default */
+    [0] = \
+    KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV, \
+           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC, \
+           LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   FN1 ,QUOT,ENT, \
+           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0, \
+                LALT,LGUI,          SPC,                RGUI,RALT),
+
+    /* HHKB Fn */
+    [1] = \
+    KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
+           CAPS,TRNS,MPRV,MNXT,MPLY,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, BSPC, \
+           TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,PENT, \
+           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS, \
+                TRNS,TRNS,          TRNS,               TRNS,TRNS),
+
+    /* Mouse */
+    [2] = \
+    KEYMAP(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+           TRNS,FN2,TRNS,TRNS,TRNS,TRNS,WH_L,WH_D,WH_U,WH_R,TRNS,TRNS,TRNS,TRNS, \
+           TRNS,TRNS,ACL0,ACL1,ACL2,TRNS,MS_L,MS_D,MS_U,MS_R,TRNS,TRNS,TRNS, \
+           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,BTN2,BTN1,TRNS,TRNS,TRNS,TRNS,TRNS, \
+                TRNS,TRNS,          BTN1,               TRNS,TRNS),
+};
+
+const action_t fn_actions[] PROGMEM = {
+    [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_LAYER_TAP_KEY(2, KC_SCOLON),
+};
