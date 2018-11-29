9f27be7d4bd14a81499bbf50b8189fc08b2cef57
Patrick Oscity <patrick.oscity@cokron.com>
2018-11-29 15:18:03 +0100

    JS configure eslint

---
+++ eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2015
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ]
+  }
+};
